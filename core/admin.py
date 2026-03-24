ffbac166c4dd4aa6d5ed78f6ae041c08d2a08de7
ablacs <ablacsxd@gmail.com>
2026-03-23 22:05:20 -0300

    Adiciona e configura o slut para ser criado junto ao título da postagem

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Post
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    
 admin.site.register(Post)
